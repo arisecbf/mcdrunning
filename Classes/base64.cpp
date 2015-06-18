f392b23a588117a87614122e36bf99ec7c179343
Chen Bingfeng <iichenbf@gmail.com>
2015-06-18 23:11:55 +0800

    add encrypt AES base64

---
+++ Classes/base64.cpp
@@ -0,0 +1,9 @@
+//
+//  base64.cpp
+//  mcdrunning
+//
+//  Created by chenbingfeng on 15/6/18.
+//
+//
+
+#include "base64.h"
---
+++ Classes/base64.h
@@ -0,0 +1,14 @@
+//
+//  base64.h
+//  mcdrunning
+//
+//  Created by chenbingfeng on 15/6/18.
+//
+//
+
+#ifndef __mcdrunning__base64__
+#define __mcdrunning__base64__
+
+#include <stdio.h>
+
+#endif /* defined(__mcdrunning__base64__) */
